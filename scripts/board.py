9c8ba9c69e9423a41e3c334897a4af865949a44d
teddyalexandre <teddyalexandre1@gmail.com>
2024-11-05 19:40:37 +0100

    add game over render function

--- scripts/board.py
+++ scripts/board.py
@@ -27,11 +27,8 @@ clock = pygame.time.Clock()
 
 def draw_board():
     """
-    Function which draws the Tetris board, without any Tetrimino"""
-
-    # Fill the whole background
-    screen.fill(BLACK)
-
+    Function which draws the Tetris board, without any Tetrimino
+    """
     
     # Draw entire board area including padding
     for row in range(NUM_ROWS + 2 * PADDING):
@@ -39,15 +36,22 @@ def draw_board():
             x = col * CELL_SIZE
             y = row * CELL_SIZE
 
-            # Check if cell is in the padding area
+            # Check if cell is in the padding area, draw in gray
             if row < PADDING or row >= NUM_ROWS + PADDING or col < PADDING or col >= NUM_COLS + PADDING:
                 pygame.draw.rect(screen, GRAY, (x, y, CELL_SIZE, CELL_SIZE))
+            # else fill with black rectangles
             else:
                 pygame.draw.rect(screen, BLACK, (x, y, CELL_SIZE, CELL_SIZE))
             
             # Draw cell border in white
             pygame.draw.rect(screen, WHITE, (x, y, CELL_SIZE, CELL_SIZE), 1)
 
+def print_end_game():
+    """Displays "Game Over!" whenever the player lost"""
+    font = pygame.font.Font(None, 80)  # Set font and size
+    text = font.render("Game Over!", True, "red", "white")
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    screen.blit(text, text_rect)
 
 # Main game loop
 while True:
@@ -57,4 +61,5 @@ while True:
             sys.exit()
     
     draw_board()
+
     pygame.display.flip()
