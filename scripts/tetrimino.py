1c8846d83910aa327ac7174006643cf3c238ca82
teddyalexandre <teddyalexandre1@gmail.com>
2024-11-07 18:20:43 +0100

    encoded the tetriminos and their rotations

--- scripts/tetrimino.py
+++ scripts/tetrimino.py
@@ -2,9 +2,32 @@
 The goal of this file is to represent the pieces of the game : the Tetriminos
 """
 
+# mapping between a letter and its color
+COLORS = {
+    'I': "aqua",
+    'O': "yellow",
+    'T': "purple",
+    'L': "orange",
+    'J': "blue",
+    'Z': "red",
+    "S": "green"
+}
+
+SHAPES = {
+    "I": [
+        [[1, 1, 1, 1]],
+        [[1], [1], [1], [1]]
+    ],
+    "O": [
+        [[0, 1, 1, 0], [0, 1, 1, 0]]
+    ],
+
+}
+
 class Tetrimino:
     def __init__(self) -> None:
         pass
+
 
     def rotatePiece(self):
         pass
